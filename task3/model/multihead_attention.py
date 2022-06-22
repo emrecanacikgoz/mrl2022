ed3cabbea96713de1b4337bea2475db1c10c91df
ecacikgoz97 <ecacikgoz97@gmail.com>
2022-06-22 16:53:29 +0300

    model added

---
+++ task3/model/multihead_attention.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+def attention(q, k, v, d_k, mask, dropout):
+
+    att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(d_k))
+
+    if mask is not None:
+        mask = mask.unsqueeze(1)
+        att  = att.masked_fill(mask == 0, -1e9)
+
+    normalized_attention = F.softmax(att, dim=-1)
+    #att = dropout(normalized_attention)
+
+    y = normalized_attention @ v
+    return y    
+
+class MultiHead_Attention(nn.Module):
+    def __init__(self, embed_dim=512, num_heads=8, dropout_rate=0.1):
+        super().__init__()
+
+        self.num_heads = num_heads
+        self.d_k       = embed_dim//num_heads
+        self.key       = nn.Linear(embed_dim, embed_dim)
+        self.query     = nn.Linear(embed_dim, embed_dim)
+        self.value     = nn.Linear(embed_dim, embed_dim)
+        self.dropout   = nn.Dropout(dropout_rate)
+        self.fc        = nn.Linear(embed_dim, embed_dim)
+        
+
+    def forward(self, q, k, v, mask=None):
+        Bq, Tq, Cq = q.shape
+        #Bk, Tk, Ck = k.shape
+        #Bv, Tv, Cv = v.shape
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q = self.query(q).view(Bq, -1, self.num_heads, Cq//self.num_heads).transpose(1, 2) # (B, num_heads, T, C//num_heads)
+        k = self.key(k).view(  Bq, -1, self.num_heads, Cq//self.num_heads).transpose(1, 2) # (B, num_heads, T, C//num_heads)
+        v = self.value(v).view(Bq, -1, self.num_heads, Cq//self.num_heads).transpose(1, 2) # (B, num_heads, T, C//num_heads)
+
+        #y = attention @ v # (B, num_heads, T, T) x (B, num_heads, T, C//num_heads) ===> (B, num_heads, T, C//num_heads)
+        y = attention(q, k, v, self.d_k, mask, self.dropout)
+        y = y.transpose(1, 2).contiguous().view(Bq, -1, Cq) # re-assemble all head outputs side by side (concat)
+        
+        # output projection
+        y = self.fc(y)
+
+        return y
---
+++ task3/model/sublayers.py
@@ -0,0 +1,37 @@
+import torch, math
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
+class Feed_Forward(nn.Module):
+    def __init__(self, embed_dim=512, expand_ratio=4):
+        super(Feed_Forward, self).__init__()
+
+        self.linear1 = nn.Linear(embed_dim, embed_dim*expand_ratio)
+        self.relu    = nn.ReLU(inplace=True)
+        self.linear2 = nn.Linear(embed_dim*expand_ratio, embed_dim)
+
+    def forward(self, x):
+
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        return x
