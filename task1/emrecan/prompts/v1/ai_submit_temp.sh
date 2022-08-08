29cdc1ea52413bfc7e7bc44e0ab723f8b9889985
ecacikgoz97 <ecacikgoz97@gmail.com>
2022-08-08 13:20:53 +0300

    prompts for task1 added

---
+++ task1/emrecan/prompts/v1/ai_submit_temp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=eacikgoz17_task1_tur_run3 # Job name
+#SBATCH --nodes=1 # Run on a single node
+#SBATCH --ntasks-per-node=4
+#SBATCH --partition=ai # Run in ai queue
+#SBATCH --qos=ai 
+#SBATCH --account=ai 
+#SBATCH --gres=gpu:tesla_t4:1 
+#SBATCH --mem=20G 
+#SBATCH --time=7-0:0:0 # Time limit days-hours:minutes:seconds
+#SBATCH --output=test-%j.out # Standard output and error log
+#SBATCH --mail-type=ALL # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=eacikgoz17@ku.edu.tr # Where to send mail
+
+
+echo "Setting stack size to unlimited..."
+ulimit -s unlimited
+ulimit -l unlimited
+ulimit -a
+echo
+
+module load anaconda/3.6
+source activate eacikgoz17
+nvidia-smi
+python conditional_generation_inf.py --lang tur --run 3
+
+source deactivate
